595632cf9b88d5bb7e2554f86705289c5c42d330
Mouchon <william.mouchon@edu.devinci.fr>
2022-02-22 11:35:26 +0100

    recuperation :wrench:

--- index.php
+++ index.php
@@ -17,20 +17,12 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<?php
-		if ( have_posts() ) :
 
-			get_template_part( 'loop' );
-
-		else :
-
-			get_template_part( 'content', 'none' );
-
-		endif;
 		?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-do_action( 'storefront_sidebar' );
+
 get_footer();
